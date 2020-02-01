9c4e6c59cb5454a0499873030e58119c9b25ee37
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2020-02-01 15:30:43 +0100

    front end first real ok state

--- srcs/html_conv_object.js
+++ srcs/html_conv_object.js
@@ -10,11 +10,12 @@ class ConvObject {
         this.conversations[conv._id] = conv;
         p.classList.add("clickable", "conv_name_list");
         p.onclick = e=>{
-            console.log(e.target)
+            // console.log(e.target)
+            this.loadConv(e.target.convID);
             this.hideConvList();
         }
         p.innerText = conv.conv_name;
-        p.targetID = conv._id;
+        p.convID = conv._id;
         convList.appendChild(p);
     }
     hideConvList() {
@@ -23,5 +24,24 @@ class ConvObject {
         document.body.classList.replace("body_show_list", "body_show_conv");
         choseConv.style.display = "none";
         document.getElementById("conversation").style.display = "grid";
+    }
+    loadConv(convID) {
+        const currentConv = this.conversations[convID];
+        
+        this.current = convID;
+        document.getElementById("conv_name").innerText = currentConv.conv_name;
+        document.getElementById("msg_list").innerHTML = ""; // on vide la liste de message avant de la remplir a nouveau
+        for (let i = 0; i < currentConv.conv_data.length; i++) {
+           this.newMsg(currentConv[i]);
+        }
+    }
+    newMsg(msgObj) {
+        const msgList = document.getElementById("msg_list");
+        const p = document.createElement("p");
+        const time = new Date(msgObj.time);
+
+        p.innerText = `${time.getHours()}h${time.getMinutes()} - ${msgObj.user_id} : ${msgObj.user_msg}`;
+        // p.classList.add()
+        msgList.appendChild(p);
     }
 }
