4ef36c773f9b0b34a8f7f43411a8e60c9be5dc61
Mayeul LE MONIES DE SAGAZAN <mle-moni@e1r6p8.42.fr>
2020-02-05 15:00:20 +0100

    V-0.4

--- srcs/html_conv_object.js
+++ srcs/html_conv_object.js
@@ -11,51 +11,60 @@ class ConvObject {
             }
         });
         document.getElementById("add_conv_button").onclick = e=>{
-            document.body.classList.replace("body_show_list", "body_show_create");
-            document.getElementById("create_conv").style.display = "block";
-            document.getElementById("chose_conv").style.display = "none";
+            console.log("cette fonction n'est pas encore disponible !")
+            // document.body.classList.replace("body_show_list", "body_show_create");
+            // document.getElementById("create_conv").style.display = "block";
+            // document.getElementById("chose_conv").style.display = "none";
         };
-        document.getElementById("input_get_users").addEventListener("keyup", e=>{
-            this.socket.emit("getUsers", document.getElementById("input_get_users").value);
-        });
-        let self = this;
-        document.getElementById("create_conv_submit").addEventListener("click", e=>{
-            self.createConv();
-        });
+        // document.getElementById("input_get_users").addEventListener("keyup", e=>{
+        //     this.socket.emit("getUsers", document.getElementById("input_get_users").value);
+        // });
+        // let self = this;
+        // document.getElementById("create_conv_submit").addEventListener("click", e=>{
+        //     self.createConv();
+        // });
     }
     newConv(conv) {
         const convList = document.getElementById("conv_list");
         const p = document.createElement("p");
+        const list = document.getElementsByClassName("conv_name_list");
+        let addConvToList = true;
 
         // on met a jour la table des utilisateurs
         for (let i = 0; i < conv.conv_users.length; i++) {
             this.socket.emit("getUserById", conv.conv_users[i]);
         }
-        this.conversations[conv._id] = conv;
-        p.classList.add("clickable", "conv_name_list");
-        // charge la conversation quand on clique sur le conv_name
-        p.onclick = e=>{
-            // console.log(e.target)
-            this.loadConv(e.target.convID);
-            this.showConv();
+        for (let i = 0; i < list.length; i++) {
+            if (list[i].convID === conv._id) {
+                addConvToList = false;
+            }
         }
-        p.innerText = conv.conv_name;
-        p.convID = conv._id;
-        convList.appendChild(p);
+        if (addConvToList) {
+            p.classList.add("clickable", "conv_name_list");
+            // charge la conversation quand on clique sur le conv_name
+            p.onclick = e=>{
+                // console.log(e.target)
+                this.loadConv(e.target.convID);
+                this.showConv();
+            }
+            p.innerText = conv.conv_name;
+            p.convID = conv._id;
+            convList.appendChild(p);
+        }
+        this.conversations[conv._id] = conv;
     }
     showConv() {
-        const choseConv = document.getElementById("chose_conv");
-
-        document.body.classList.replace("body_show_list", "body_show_conv");
-        choseConv.style.display = "none";
-        document.getElementById("conversation").style.display = "grid";
+        if (window.innerWidth <= 700) {
+            toggleMenuVisibility();
+        }
         document.getElementById("msg_list").scroll(0, 10000000);
     }
     loadConv(convID) {
         const currentConv = this.conversations[convID];
         
         this.current = convID;
-        document.getElementById("conv_name").innerText = currentConv.conv_name;
+        document.getElementById("conv_name").getElementsByTagName("h1")[0].innerText = currentConv.conv_name;
+        console.log("ajouter la liste d'utilisateurs")
         document.getElementById("msg_list").innerHTML = ""; // on vide la liste de message avant de la remplir a nouveau
         for (let i = 0; i < currentConv.conv_data.length; i++) {
            this.newMsg(currentConv.conv_data[i]);
@@ -63,11 +72,25 @@ class ConvObject {
     }
     newMsg(msgObj) {
         const msgList = document.getElementById("msg_list");
-        const p = document.createElement("p");
+        const msg = document.createElement("article");
         const userName = this.usersTable[msgObj.user_id] || "Unknown user";
-        p.innerText = `${this.getFormatedDate(new Date(msgObj.time))} - ${userName} : ${msgObj.user_msg}`;
-        // p.classList.add()
-        msgList.appendChild(p);
+
+        msg.innerHTML = `<h5></h5>
+        <span>
+            <div style="display: flex;">
+                <img src="img/unknown_user.png" />
+                <div style="margin: auto;"><b class="msg-psd"></b></div>
+            </div>
+            <p></p>
+        </span>`;
+        msg.getElementsByTagName("h5")[0].textContent = this.getFormatedDate(new Date(msgObj.time));
+        msg.getElementsByClassName("msg-psd")[0].textContent = userName;
+        msg.getElementsByTagName("p")[0].textContent = msgObj.user_msg;
+        if (userName === sessionStorage.psd) {
+            msg.classList.add("from-me");
+        }
+        msgList.appendChild(msg);
+        
         msgList.scroll(0, 10000000);
     }
     getFormatedDate(date) {
