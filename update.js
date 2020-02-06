32f776b2a7c149f119f2fb3dcad75bac7c6156fe
Mayeul LE MONIES DE SAGAZAN <mle-moni@e2r6p7.42.fr>
2020-02-06 16:28:56 +0100

    please work, worker..

--- update.js
+++ update.js
@@ -11,8 +11,7 @@ self.addEventListener('notificationclick', function (event) {
 		.then(function (clientList) {
 			for (var i = 0; i < clientList.length; i++) {
 				var client = clientList[i];
-				console.log(client.url)
-				if (client.url == '/' && 'focus' in client)
+				if (client.url == 'https://messenger.renouv.art/' && 'focus' in client)
 					return client.focus();
 			}
 			if (clients.openWindow) {
