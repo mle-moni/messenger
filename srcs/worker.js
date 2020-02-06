087fee93ec3d06422fd6b4ac6b1428b3ec576e84
Mayeul LE MONIES DE SAGAZAN <mle-moni@e2r6p7.42.fr>
2020-02-06 15:25:38 +0100

    worker test

--- srcs/worker.js
+++ srcs/worker.js
@@ -1,20 +1,24 @@
-self.addEventListener('notificationclick', function(event) {
-    let url = 'https://messenger.renouv.art/test.html';
-    event.notification.close(); // Android needs explicit close.
-    event.waitUntil(
-        self.clients.matchAll({type: 'window'}).then( windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                // If so, just focus it.
-                if ('focus' in client) {
-                    return client.focus();
-                }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (self.clients.openWindow) {
-                return self.clients.openWindow(url);
-            }
-        })
-    );
-});
+self.addEventListener('notificationclick', function(event) {  
+	console.log('On notification click: ', event.notification.tag);
+	// Android doesn't close the notification when you click on it  
+	// See: http://crbug.com/463146  
+	event.notification.close();
+  
+	// This looks to see if the current is already open and  
+	// focuses if it is  
+	event.waitUntil(
+	  clients.matchAll({  
+		type: "window"  
+	  })
+	  .then(function(clientList) {  
+		for (var i = 0; i < clientList.length; i++) {  
+		  var client = clientList[i];  
+		  if (client.url == '/' && 'focus' in client)  
+			return client.focus();  
+		}  
+		if (clients.openWindow) {
+		  return clients.openWindow('https://deanhume.github.io/typography');
+		}
+	  })
+	);
+  });
