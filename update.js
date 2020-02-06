40a8406e67893b86371a5b8a05edf911d38230cd
Mayeul LE MONIES DE SAGAZAN <mle-moni@e2r6p7.42.fr>
2020-02-06 16:14:59 +0100

    Worker update, should work fine

--- update.js
+++ update.js
@@ -1,14 +1,27 @@
 self.addEventListener('notificationclick', function (event) {
+	// close on notif click
 	event.notification.close();
-	clients.openWindow("https://youtu.be/PAvHeRGZ_lA");
-  });
 
-self.addEventListener('fetch', function(event) {
-// event.respondWith(
-// 	caches.match(event.request).catch(function() {
-// 		return fetch(event.request);
-// 	})
-// );
+	// This looks to see if the current is already open and
+	// focuses if it is
+	event.waitUntil(
+		clients.matchAll({
+			type: "window"
+		})
+		.then(function (clientList) {
+			for (var i = 0; i < clientList.length; i++) {
+				var client = clientList[i];
+				if (client.url == '/' && 'focus' in client)
+					return client.focus();
+			}
+			if (clients.openWindow) {
+				return clients.openWindow('https://messenger.renouv.art/test.html');
+			}
+		})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
 	event.respondWith(
 		fetch(event.request)
 	)
